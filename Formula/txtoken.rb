473100cbc7b46d55cb630c3cf170b3ee73de87de
Craig Johnston <cjimti@gmail.com>
2018-05-26 23:36:54 -0700

    Brew formula update for txtoken version v1.0.4

--- Formula/txtoken.rb
+++ Formula/txtoken.rb
@@ -1,9 +1,9 @@
 class Txtoken < Formula
   desc "JWT token authentication proxy."
   homepage "https://github.com/txn2/txtoken"
-  url "https://github.com/txn2/txtoken/releases/download/v1.0.3/txtoken_darwin_amd64.tar.gz"
-  version "1.0.3"
-  sha256 "5a13c38922f8aebab3a08fe6d60c553f02e35f6071f09610e9b4091a54c86d6b"
+  url "https://github.com/txn2/txtoken/releases/download/v1.0.4/txtoken_darwin_amd64.tar.gz"
+  version "1.0.4"
+  sha256 "d12ab565eb72e630e67e35ac098e9d9dcb2980538e3955849c821b0a4d67ddc0"
 
   def install
     bin.install "txtoken"
