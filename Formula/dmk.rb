5ea19ff3a03ac5b032e71f51823316dce6f31655
Craig Johnston <cjimti@gmail.com>
2018-05-29 23:09:48 -0700

    Brew formula update for dmk version v1.0.2

--- Formula/dmk.rb
+++ Formula/dmk.rb
@@ -1,9 +1,9 @@
 class Dmk < Formula
   desc "Declarative data migration in yaml (with embedded javascript support.)"
   homepage "https://github.com/txn2/dmk"
-  url "https://github.com/txn2/dmk/releases/download/v1.0.1/dmk_macOS_amd64.tar.gz"
-  version "1.0.1"
-  sha256 "34afebef3bd7a5f8022112192cd0dcf1d00ac1dee347ae365b6d627b590caaec"
+  url "https://github.com/txn2/dmk/releases/download/v1.0.2/dmk_macOS_amd64.tar.gz"
+  version "1.0.2"
+  sha256 "48541b0b3a7c15c63a30d52839ecbb926fbb6659c6511417bcb1bdb900f16c58"
 
   def install
     bin.install "dmk"
