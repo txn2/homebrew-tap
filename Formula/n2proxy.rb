6d90519937a6cc5398a9932b0aa5be0bfe75053d
Craig Johnston <cjimti@gmail.com>
2018-06-15 10:51:39 -0700

    Brew formula update for n2proxy version v1.0.3

--- Formula/n2proxy.rb
+++ Formula/n2proxy.rb
@@ -1,9 +1,9 @@
 class N2proxy < Formula
   desc "Contraband filtering reverse proxy."
   homepage "https://github.com/txn2/n2proxy"
-  url "https://github.com/txn2/n2proxy/releases/download/v1.0.2/n2proxy_macOS_amd64.tar.gz"
-  version "1.0.2"
-  sha256 "5b8a460886311f6d51c9a0a113b0a132dee2a77fa52d07c8b564bdc0441cfdf4"
+  url "https://github.com/txn2/n2proxy/releases/download/v1.0.3/n2proxy_macOS_amd64.tar.gz"
+  version "1.0.3"
+  sha256 "188b8125e281859b5e521b0f480a049e51571c072942f6768de0c497a274f216"
 
   def install
     bin.install "n2proxy"
