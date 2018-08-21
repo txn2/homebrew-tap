f0a6e1f0b0d05264593e92d6e4945940f39278e3
Craig Johnston <cjimti@gmail.com>
2018-08-20 23:56:14 -0700

    Brew formula update for n2bot version v1.0.2

--- Formula/n2bot.rb
+++ Formula/n2bot.rb
@@ -1,9 +1,9 @@
 class N2bot < Formula
   desc "Webhook to IRC bot."
   homepage "https://github.com/txn2/n2bot"
-  url "https://github.com/txn2/n2bot/releases/download/v1.0.1/n2bot_macOS_amd64.tar.gz"
-  version "1.0.1"
-  sha256 "d53a5429694425b4bc9870e5118495ca4202debd6c78dedc4f5aa8b92b8b7b16"
+  url "https://github.com/txn2/n2bot/releases/download/v1.0.2/n2bot_macOS_amd64.tar.gz"
+  version "1.0.2"
+  sha256 "9f5daeac902499d9cb479ae67c2a57f704e0e2521d2f3969c4376849e54adbaa"
 
   def install
     bin.install "n2bot"
