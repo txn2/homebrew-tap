7cfd92b0f84ad3c9a6abccf2d856893b665b8b0f
Craig Johnston <cjimti@gmail.com>
2018-11-18 00:06:52 -0800

    Brew formula update for xn2 version v1.0.1

--- Formula/kubefwd.rb
+++ Formula/kubefwd.rb
@@ -1,9 +1,9 @@
 class Kubefwd < Formula
   desc "Metrics scraper and muxer."
   homepage "https://github.com/txn2/xn2"
-  url "https://github.com/txn2/xn2/releases/download/v1.0.0/xn2_macOS_amd64.tar.gz"
-  version "1.0.0"
-  sha256 "baa5af4bd7cf4bf8c53e1fa8d01311216835c9319f6e6c3d581ff2b825be87fc"
+  url "https://github.com/txn2/xn2/releases/download/v1.0.1/xn2_macOS_amd64.tar.gz"
+  version "1.0.1"
+  sha256 "c28d9c5072957cf0436677d96d0eade1f66a1ad551aeb0b4817aabcee4d076a6"
 
   def install
     bin.install "xn2"
