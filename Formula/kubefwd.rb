f6bbcee3ba7b9a7fe19348dac21936146432160e
Craig Johnston <cjimti@gmail.com>
2018-11-17 19:56:45 -0800

    Brew formula update for xn2 version v1.0.0

--- Formula/kubefwd.rb
+++ Formula/kubefwd.rb
@@ -1,12 +1,12 @@
 class Kubefwd < Formula
-  desc "Kubernetes bulk port forwarding utility."
-  homepage "https://github.com/txn2/kubefwd"
-  url "https://github.com/txn2/kubefwd/releases/download/1.4.7/kubefwd_macOS_amd64.tar.gz"
-  version "1.4.7"
-  sha256 "75816341382067196fdb6cca508ee83c3059d215ee15ed6f7ae6cbecf4588c0d"
+  desc "Metrics scraper and muxer."
+  homepage "https://github.com/txn2/xn2"
+  url "https://github.com/txn2/xn2/releases/download/v1.0.0/xn2_macOS_amd64.tar.gz"
+  version "1.0.0"
+  sha256 "baa5af4bd7cf4bf8c53e1fa8d01311216835c9319f6e6c3d581ff2b825be87fc"
 
   def install
-    bin.install "kubefwd"
+    bin.install "xn2"
   end
 
   test do
