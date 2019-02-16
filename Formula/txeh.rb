999c753f8d475318934b0fab2773d64a9d16236c
Craig Johnston <cjimti@gmail.com>
2019-02-16 15:37:35 -0800

    Brew formula update for txeh version v1.0.2

--- Formula/txeh.rb
+++ Formula/txeh.rb
@@ -1,9 +1,9 @@
 class Txeh < Formula
   desc "Etc Hosts Management Utility."
   homepage "https://github.com/txn2/txeh"
-  url "https://github.com/txn2/txeh/releases/download/v1.0.1/txeh_macOS_amd64.tar.gz"
-  version "1.0.1"
-  sha256 "be709a3fb7234a4f7bcaa3abd677471d102582574e2c9ea75663c00a7e5e1a0f"
+  url "https://github.com/txn2/txeh/releases/download/v1.0.2/txeh_macOS_amd64.tar.gz"
+  version "1.0.2"
+  sha256 "5bc6adb61dd56681e39eb4964fe86387e6d423401d10e8fb2e6646cebc9b0e24"
 
   def install
     bin.install "txeh"
