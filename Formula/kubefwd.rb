f16c84b25ed55ee310911e26b460a9edaa313d8d
Craig Johnston <cjimti@gmail.com>
2020-04-21 09:48:50 -0700

    Brew formula update for kubefwd version 1.13.1

--- Formula/kubefwd.rb
+++ Formula/kubefwd.rb
@@ -2,9 +2,9 @@
 class Kubefwd < Formula
   desc "Kubernetes bulk port forwarding utility."
   homepage "https://github.com/txn2/kubefwd"
-  url "https://github.com/txn2/kubefwd/releases/download/v1.13.1/kubefwd_Darwin_amd64.tar.gz"
+  url "https://github.com/txn2/kubefwd/releases/download/1.13.1/kubefwd_Darwin_amd64.tar.gz"
   version "1.13.1"
-  sha256 "56b6fc2e2cfa00ade21102ec4da52a1eda3299933038850eba3a254f2af2c206"
+  sha256 "3fc71b4a8e61a99e1e8b3e2148dda405ea2997f76103191703c8afda61877230"
 
   def install
     bin.install "kubefwd"
